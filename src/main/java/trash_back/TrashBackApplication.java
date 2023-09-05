5274824671653c6828e047ee3acb57994eea9914
Hannes Soosaar <hannes.study@gmail.com>
2023-09-05 15:13:57 +0300

    Project Creation and upload (JPA commented out)

---
+++ src/main/java/trash_back/TrashBackApplication.java
@@ -0,0 +1,13 @@
+package trash_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashBackApplication.class, args);
+	}
+
+}
