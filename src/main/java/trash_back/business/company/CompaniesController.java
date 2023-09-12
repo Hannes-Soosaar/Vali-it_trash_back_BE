735eaea42ddca0a38c145423ec841a39376d9707
TAIRI TIIMANN <tairiti@gmail.com>
2023-09-12 09:13:58 +0300

    teenus POST - kasutaja loomine valmis

--- src/main/java/trash_back/business/company/CompaniesController.java
+++ src/main/java/trash_back/business/company/CompaniesController.java
@@ -1,9 +1,15 @@
 package trash_back.business.company;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 import trash_back.business.company.dto.CompanyInfo;
 import trash_back.business.company.dto.CompanyRequest;
+import trash_back.infrastructure.error.ApiError;
 
 @RestController
 public class CompaniesController {
@@ -20,6 +26,13 @@ public class CompaniesController {
     }
 
     @PostMapping("/company")
+    @Operation(summary = "Uue kasutaja registreerimine",
+            description = """
+                Sisse tuleb email, parool, ettevõtte nimi ja ettevõtte reg nr. Need salvestatakse andmebaasi.""")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "", description = "",
+                    content = @Content(schema = @Schema(implementation = ApiError.class)))})
 
     public void addUserAndCompany(@RequestBody CompanyRequest companyRequest) {
 
